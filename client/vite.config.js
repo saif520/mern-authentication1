489458c13d0314255c2aff9194dfec7f3abfcd0c
saif520 <62957608+saif520@users.noreply.github.com>
2025-03-15 00:48:32 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Ensures Vite binds to the correct IP
-    port: process.env.PORT || 3000 // Ensures dynamic port assignment
-  }
-});
+  plugins: [react()],
+})
+
