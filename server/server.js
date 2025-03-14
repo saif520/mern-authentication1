09f81d74c837e35aecadffbc3bb8cafa20047ee4
saif520 <62957608+saif520@users.noreply.github.com>
2025-03-15 00:12:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import { app } from "./app.js";
 
-app.listen(process.env.PORT, () => {
+const PORT=process.env.PORT||3000
+
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
