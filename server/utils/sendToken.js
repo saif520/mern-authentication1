54e0ed6861536d1deeb4c67f7b1e97a4726266ad
saif520 <62957608+saif520@users.noreply.github.com>
2025-03-15 13:02:00 +0530

    Update sendToken.js

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -3,10 +3,11 @@ export const sendToken = (user, statusCode, message, res) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      path: "/", 
     })
     .json({
       success: true,
